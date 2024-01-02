3a8526c9bfa68579da5e4823e20b3dc36ea2d49e
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-02 11:16:53 +0600

    move file

--- CF Problem Solve/A_Boy_or_Girl.cpp
+++ CF Problem Solve/A_Boy_or_Girl.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     string s;
     cin >> s;
     sort(s.begin(), s.end());
-    int l = s.length();
-    int ans = 0;
-    for (int i = 1; i < l; i++)
+    int count_letter = 0;
+    for (int i = 1; i < s.length(); i++)
     {
         if (s[i] != s[i - 1])
-            ans++;
+            count_letter++;
     }
-    if (ans & 1)
-        cout << "CHAT WITH HER!" << endl;
-    else
-        cout << "IGNORE HIM!" << endl;
+    int without_dup = count_letter + 1;
+
+    // cout<<without_dup;
+    
+    if(without_dup%2==0)
+        cout<<"CHAT WITH HER!"<<endl;
+    else 
+        cout<<"IGNORE HIM!"<<endl;
 
     return 0;
 }
