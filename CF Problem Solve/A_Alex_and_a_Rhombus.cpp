7a165231d527114ed4ae13805a9f52ae0ff801e9
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-30 01:01:28 +0600

    OJ

---
+++ CF Problem Solve/A_Alex_and_a_Rhombus.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int result = pow(n, 2) + pow(n - 1, 2);
+    cout << result << endl;
+    return 0;
+}
