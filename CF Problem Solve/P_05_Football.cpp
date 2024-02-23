0bf18754622691eaeb44c474261c2faa75dcf5dc
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-02-23 15:11:09 +0600

    CF bug found

--- CF Problem Solve/P_05_Football.cpp
+++ CF Problem Solve/P_05_Football.cpp
@@ -3,30 +3,34 @@
 
 using namespace std;
 
-int main() {
-  string s;
-  cin >> s;
+int main()
+{
+    string s;
+    cin >> s;
 
-  int count = 0;
-  bool dangerous = false;
+    int count = 0;
+    bool dangerous = false;
 
-  for (char c : s) {
-    if (c == '0') {
-      count++;
-      if (count >= 7) {
-        dangerous = true;
-        break;
-      }
-    } else {
-      count = 0;
+    for (char c : s)
+    {
+        if (c == '0')
+        {
+            count++;
+            if (count >= 7)
+            {
+                dangerous = true;
+                break;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
     }
-  }
 
-  if (dangerous) {
-    cout << "YES" << endl;
-  } else {
-    cout << "NO" << endl;
-  }
-
-  return 0;
+    if (dangerous == true)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
 }
