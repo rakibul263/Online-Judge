568df982c2c48216654228c68131917d2a04a476
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-04-02 04:58:14 +0600

    cp

--- CF Problem Solve/N_The_First_Challenge.cpp
+++ CF Problem Solve/N_The_First_Challenge.cpp
@@ -1,21 +1,23 @@
-#include <bits/stdc++.h>
-#define ll long long int
-using namespace std;
+/******************************
+ * author: Md Rakibul Hasan
+ * date: 2024-03-23 02:58:39
+ ******************************/
 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
 
     ll n;
     cin >> n;
     ll sum = 0;
     for (ll i = 1; i <= n; i++)
     {
-        // if (i % 2 != 0)
-        sum += i;
-        i++;
+        if (i % 2 != 0)
+            sum += i;
     }
     cout << sum << '\n';
 
