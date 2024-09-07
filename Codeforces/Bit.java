a7de6a3c67bed31630a01d9357151130bb2147b8
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-07 13:48:48 +0600

    CP

---
+++ Codeforces/Bit.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int X=0;
+        while(n-- > 0){
+            String str = sc.next();
+            if (str == "X++") {
+                X++;
+            }
+            else if (str == "++X") {
+                ++X;
+            }
+            else if (str == "X--") {
+                X--;
+            }
+            else if (str == "--X") {
+                --X;
+            }
+        }
+        System.out.println(X);
+    }
+}
