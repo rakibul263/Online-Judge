cf70120951d7e5fa44621992e6dcd0d1991a4505
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-10 11:59:39 +0600

    cp

---
+++ CodeChef/IOI_2024.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class IOI_2024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>=1 && n<=8){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
