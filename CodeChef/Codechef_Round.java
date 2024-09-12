7bf75a6b797daacc6df340d61b20e127b56752b2
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-12 20:41:17 +0600

    cp

---
+++ CodeChef/Codechef_Round.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Codechef_Round {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==4){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
--- CodeChef/InternationalGymDay.java
+++ CodeChef/InternationalGymDay.java
@@ -1,2 +1,30 @@
+import java.util.*;
+
 public class InternationalGymDay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int d = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int result = solve(d,x,y);
+            System.out.println(result);
+        }
+    }
+    public static int solve(int d, int x, int y) {
+        for(int i=0;i<100;i++){
+            int remBg = y-i;
+            if (remBg < 0) {
+                return -1;
+            }
+            double dp = x*(1-((d*i)/100.0));
+            if (remBg >= dp) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
