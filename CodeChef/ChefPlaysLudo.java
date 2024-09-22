1465ef3b444aad869813991925666fe39122462d
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-22 21:17:36 +0600

    cp

---
+++ CodeChef/ChefPlaysLudo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ChefPlaysLudo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int X = sc.nextInt();
+
+            if (X == 6) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+}
