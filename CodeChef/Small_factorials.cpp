02e147773171ccc12a6bad78ba69f8a3d2cc0643
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-24 22:25:26 +0600

    cp

---
+++ CodeChef/Small_factorials.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        unsigned long long re = factorial(n);
+        cout<<re<<endl;
+    }
+
+
+clock_off
+
+    return 0;
+}
