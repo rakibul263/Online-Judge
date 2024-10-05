02a799e65eb05aebad0b2271e096c74e1a21de1a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-05 22:13:26 +0600

    cp

---
+++ Codeforces/Area_of_circle.cpp
@@ -0,0 +1,47 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+// #define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define PI             3.141592653
+
+
+void solve(){
+    double num;
+    cin>>num;
+
+    double area = PI*num*num;
+    cout<<fixed<<setprecision(9)<<area<<endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+clock_off
+
+    return 0;
+}
