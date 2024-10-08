95f40fc29f687146e3eee48862c4c6cd410c8793
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-08 22:55:15 +0600

    cp

---
+++ Codeforces/Max_and_Min.cpp
@@ -0,0 +1,52 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+void solve(){
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    int Minimum = min(a, min(b,c));
+    cout<<Minimum<<" ";
+
+    int Maximum = max(a, max(b,c));
+    cout<<Maximum<<endl;
+    
+
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+    
+
+
+clock_off
+
+    return 0;
+}
