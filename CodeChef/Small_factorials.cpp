31cbc166258565ed368ecbdb69716415bc73b905
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-14 11:59:56 +0600

    cp

--- CodeChef/Small_factorials.cpp
+++ CodeChef/Small_factorials.cpp
@@ -1,7 +1,9 @@
+/***** Preprocessor Directive *****/
 #include<bits/stdc++.h>
 
 using namespace std;
 
+/***** Macros -> Symbolic Constants *****/
 #define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define clock_on       clock_t z = clock();
 #define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
@@ -12,12 +14,35 @@ using namespace std;
 #define vi             vector<int>
 #define print(a)       for(auto x : a) cout << x << " "; cout << endl
 
-unsigned long long factorial(int n) {
-    unsigned long long result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
+void multiply(vector<int> &ans, int x) {
+    int carry = 0;
+    for (int i = 0; i < ans.size(); i++) {
+        int p = ans[i] * x + carry;
+        ans[i] = p % 10;  
+        carry = p / 10; 
     }
-    return result;
+    while (carry) {
+        ans.push_back(carry % 10);
+        carry /= 10;
+    }
+}
+
+void large_factorial(int n) {
+    vector<int> ans(1, 1);
+    for (int i = 2; i <= n; i++) {
+        multiply(ans, i); 
+    }
+    
+    for (int i = ans.size() - 1; i >= 0; i--) {
+        cout << ans[i];
+    }
+    cout << endl;
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    large_factorial(n); 
 }
 
 /*****Main Function*****/
@@ -34,13 +59,7 @@ clock_on
 
     int t = 1;
     cin>>t;
-    while(t--) {
-        int n;
-        cin>>n;
-        unsigned long long re = factorial(n);
-        cout<<re<<endl;
-    }
-
+    while(t--) solve();
 
 clock_off
 
