92fd6501b7d0409ab65b6152230eaff663b944f7
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-16 19:53:49 +0600

    cp

---
+++ Codeforces/GCD.cpp
@@ -0,0 +1,58 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void solve(){
+    int a, b;
+    cin>>a>>b;
+
+    cout<<gcd(a,b)<<" ";
+    
+
+}
+
+
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
