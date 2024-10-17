fbf9c76eb72605cdfe0acc323afc832bcb526a3d
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-17 17:55:28 +0600

    cp

---
+++ Codeforces/ Lucky_Numbers.cpp
@@ -0,0 +1,63 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+bool lucky_number(int num){
+    while(num > 0){
+        int rem = num % 10;
+        if(rem != 4 && rem != 7){
+            return false;
+        }
+        num /= 10;
+    }
+    return true;
+}
+
+void solve(){
+    int a, b;
+    cin>>a>>b;
+    bool found = false;
+
+    for(int i=a; i<=b; i++){
+        if(lucky_number(i)){
+            cout<<i<<" ";
+            found = false;
+        }
+    }
+    if(found == true)
+        cout<< -1 <<endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+clock_off
+
+    return 0;
+}
