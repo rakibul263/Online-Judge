e1987aea0e1ff80874c8b251ac8875eee8cb7653
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-18 21:17:43 +0600

    cp

---
+++ Codeforces/Sequence_of_Numbers_and_Sum.cpp
@@ -0,0 +1,67 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+void solve(){
+
+    while(true){
+        int a, b;
+        cin>>a>>b;
+        if(a <= 0 || b <= 0){
+            break;
+        }
+        else{
+            if(a>b){
+                int sum = 0;
+                for(int i=b;i<=a;i++){
+                    cout<<i<<" ";
+                    sum += i;
+                }
+                cout<<"sum ="<<sum<<endl;
+            }else{
+                int sum = 0;
+                for(int i=a;i<=b;i++){
+                    cout<<i<<" ";
+                    sum += i;
+                }
+                cout<<"sum ="<<sum<<endl;
+            }
+        }
+    }
+
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
