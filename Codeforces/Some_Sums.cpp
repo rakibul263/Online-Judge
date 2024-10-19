38e7605b949c34ffa05860fe7b1e7300653d1498
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-19 20:18:49 +0600

    cp

---
+++ Codeforces/Some_Sums.cpp
@@ -0,0 +1,61 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+int sum_of_digits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+void solve(){
+    int N, A, B;
+    cin >> N >> A >> B;
+    
+    int result = 0;
+    
+    for (int i = 1; i <= N; i++) {
+        int sum = sum_of_digits(i);
+        if (sum >= A && sum <= B) {
+            result += i;
+        }
+    }
+    
+    cout << result << endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+clock_off
+
+    return 0;
+}
---
+++ Codeforces/SumofConsecutiveOddNumbers.cpp
@@ -0,0 +1,60 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+
+void solve(){
+    int a, b;
+    cin>>a>>b;
+
+    int sum = 0;
+    if(a>b){
+        for(int i=b+1;i<a;i++){
+            if(i%2 == 1){
+                sum+=i;
+            }
+        }
+    }else{
+        for(int i=a+1;i<b;i++){
+            if(i%2 == 1){
+                sum+=i;
+            }
+        }
+    }
+    cout<<sum<<endl;
+
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
