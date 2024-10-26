749ab44f331491ed08711c9bc7904244054a8ccf
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-26 20:26:27 +0600

    cp

---
+++ Codeforces/Matrix.cpp
@@ -0,0 +1,62 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+void solve(){
+    int n;
+    cin>>n;
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int pri_sum = 0, sec_sum = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) pri_sum += arr[i][j];
+            if(i+j == n-1) sec_sum += arr[i][j];
+        }
+    }
+
+    cout<<abs(pri_sum-sec_sum)<<endl;
+
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
