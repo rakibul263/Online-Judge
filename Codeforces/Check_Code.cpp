413bdb857092b42adf1adb924427d8a2de401081
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-26 19:28:49 +0600

    cp

---
+++ Codeforces/Check_Code.cpp
@@ -0,0 +1,62 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    
+    string s;
+    cin >> s;
+    
+    if (s[a] != '-') {
+        cout << "No" << endl;
+        return;
+    }
+    
+    for (int i = 0; i < s.length(); ++i) {
+        if (i != a && !isdigit(s[i])) {
+            cout << "No" << endl;
+            return;
+        }
+    }
+    
+    cout << "Yes" << endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
---
+++ Codeforces/Fibonacci.cpp
@@ -0,0 +1,52 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define ull            unsigned long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+ull fibo(int n){
+    if(n==1) return 0;
+    if(n==2) return 1;
+    return fibo(n-1)+fibo(n-2);
+}
+
+void solve(){
+    ull n;
+    cin>>n;
+    cout<<fibo(n)<<endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
