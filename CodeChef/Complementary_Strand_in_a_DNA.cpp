db6fa164cc08554c9421b77a02817339fa8a4cae
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-11-22 19:39:31 +0600

    cp

---
+++ CodeChef/Complementary_Strand_in_a_DNA.cpp
@@ -0,0 +1,55 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+void solve(){
+    int n;
+    cin >> n;
+    char arr[n];
+    lp(n) cin >> arr[i];
+
+    for(int i=0; i<n; i++){
+        if(arr[i] == 'A') cout << 'T';
+        else if(arr[i] == 'T') cout << 'A';
+        else if(arr[i] == 'C') cout << 'G';
+        else cout << 'C';
+    }
+    cout << endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
