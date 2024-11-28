6e72c5331f9dbf851d996c1c8f87827914b2ebe2
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-11-28 22:44:49 +0600

    cp

---
+++ CodeChef/Blackjack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        int c = 21 - (a + b);
+        if (c >= 1 && c <= 10) {
+            cout << c << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
