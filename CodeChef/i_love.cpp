9a99d2a83f05c27d7b780e86f4843c45b5a2a5ee
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-11-30 23:24:01 +0600

    cp

---
+++ CodeChef/i_love.cpp
@@ -0,0 +1,66 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> points(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> points[i];
+    }
+
+    int amazingCount = 0;
+    int minPoints = points[0];
+    int maxPoints = points[0];
+
+    for (int i = 1; i < n; i++) {
+        if (points[i] > maxPoints) {
+            amazingCount++;
+            maxPoints = points[i];
+        } else if (points[i] < minPoints) {
+            amazingCount++;
+            minPoints = points[i];
+        }
+    }
+
+    cout << amazingCount << endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
