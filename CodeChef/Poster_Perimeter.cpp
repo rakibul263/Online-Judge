04e74e7f64d3a1c727e8b8eaff98ff2ec8888ca4
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-01 19:52:53 +0600

    cp

---
+++ CodeChef/Poster_Perimeter.cpp
@@ -0,0 +1,56 @@
+/***** Preprocessor Directive *****/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***** Macros -> Symbolic Constants *****/
+#define fast_io        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clock_on       clock_t z = clock();
+#define clock_off      z = clock() - z;  cerr << "Run Time : " << static_cast<double>(z) / CLOCKS_PER_SEC << " seconds" << endl;
+#define int            long long int
+#define double         long double
+#define endl           "\n"
+#define pb             push_back
+#define vi             vector<int>
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define arr_in(n)      int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define lp(n)          for(int i=0;i<n;i++)
+
+void solve(){
+    int n, m, k;
+    cin>>n>>m>>k;
+
+    int min_dif = INT_MAX;
+
+    for(int l = 1; l <= n; l++){
+        for(int w = 1; w <= m; w++){
+            int p = 2 * (l + w);
+            int dif = abs(p - k);
+            min_dif = min(min_dif, dif);
+        }
+    }
+    cout<<min_dif<<endl;
+}
+
+/*****Main Function*****/
+int32_t main(){
+
+    fast_io
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+clock_on
+
+    int t = 1;
+    cin>>t;
+    while(t--) solve();
+
+
+
+clock_off
+
+    return 0;
+}
